6542c1a61551919ae456b5be8c7afe68346e51bf
Szedlák Tamás <tamas94@protonmail.com>
2023-09-20 22:00:20 +0200

    user creation endpoint

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongo = require('./database');
 
+const { createUser } = require('./functions/createUser.js');
+
 const server = express();
 server.use(express.json());
+
+server.post('/createUser', createUser);
 
 server.listen(process.env.EXPRESSPORT, () => {
   console.log('Server is running at:' + process.env.EXPRESSPORT);
---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: false,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: false,
+    match: /^[a-zA-Z0-9]{0,50}$/,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match:
+      /^[A-Za-z0-9_]+([\.-]?[A-Za-z0-9_]+)*@[A-Za-z0-9_]+([\.-]?[A-Za-z0-9_]+)*(\.[A-Za-z0-9_]{2,3})+$/,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
