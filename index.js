bcd0e41f98835a049274c5679b70b5323f7eb398
Szedlák Tamás <tamas94@protonmail.com>
2023-09-20 22:35:46 +0200

    able to get all user

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const mongo = require('./database');
 
 const { createUser } = require('./functions/createUser.js');
+const { getAllUser } = require('./functions/getAllUser.js');
 
 const server = express();
 server.use(express.json());
 
 server.post('/createUser', createUser);
+server.get('/getAllUser', getAllUser);
 
 server.listen(process.env.EXPRESSPORT, () => {
   console.log('Server is running at:' + process.env.EXPRESSPORT);
