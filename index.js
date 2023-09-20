a2020011e0479b46404afb2ad4dbf1198b2d239c
Szedlák Tamás <tamas94@protonmail.com>
2023-09-21 00:44:38 +0200

    able to modify

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { createUser } = require('./functions/createUser.js');
 const { getAllUser } = require('./functions/getAllUser.js');
 const { getUserByID } = require('./functions/getUserByID.js');
 const { deleteUserByID } = require('./functions/deleteUserByID.js');
+const { updateUserByID } = require('./functions/updateUserByID.js');
 
 const server = express();
 server.use(express.json());
@@ -15,6 +16,7 @@ server.get('/getAllUser', getAllUser);
 //and someone could wiretape the line
 server.get('/getUserByID', getUserByID);
 server.delete('/deleteUserByID', deleteUserByID);
+server.put('/updateUserByID', updateUserByID);
 
 server.listen(process.env.EXPRESSPORT, () => {
   console.log('Server is running at:' + process.env.EXPRESSPORT);
