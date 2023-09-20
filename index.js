124ce576f34b0d0cae0b0aa62d29d492d4863a59
Szedlák Tamás <tamas94@protonmail.com>
2023-09-20 23:34:13 +0200

    able to get a user by id

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const mongo = require('./database');
 
 const { createUser } = require('./functions/createUser.js');
 const { getAllUser } = require('./functions/getAllUser.js');
+const { getUserByID } = require('./functions/getUserByID.js');
 
 const server = express();
 server.use(express.json());
 
 server.post('/createUser', createUser);
 server.get('/getAllUser', getAllUser);
+//ID send in request body, because ID could sensitive
+//and someone could wiretape the line
+server.get('/getUserByID', getUserByID);
 
 server.listen(process.env.EXPRESSPORT, () => {
   console.log('Server is running at:' + process.env.EXPRESSPORT);
